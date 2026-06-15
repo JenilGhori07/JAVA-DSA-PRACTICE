19ded09b498164008694882492c40ec40a89eb9b
Jenil Ghori <jenilghori4342@gmail.com>
2026-06-15 16:57:26 +0530

    Add files via upload

---
+++ Recursion-3rd/PermutationsOfString.java
@@ -0,0 +1,35 @@
+// String Permutations:
+// Generate and print all possible arrangements of characters
+// in a string using recursion.
+
+public class PermutationsOfString {
+
+    public static void PrintPerm(String str, String Permutation) {
+
+        // Base case: permutation is complete
+        if (str.length() == 0) {
+            System.out.println(Permutation);
+            return;
+        }
+
+        // Select each character one by one
+        for (int i = 0; i < str.length(); i++) {
+
+            char Current = str.charAt(i);
+
+            // Create a new string by removing the selected character
+            String Newstr = str.substring(0, i) + str.substring(i + 1);
+
+            // Recursive call for remaining characters
+            PrintPerm(Newstr, Permutation + Current);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String str = "abc";
+
+        // Generate all permutations of the string
+        PrintPerm(str, "");
+    }
+}
