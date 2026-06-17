2a73bb6e1a0f32fc01c4d88335df9410a180d027
Jenil Ghori <jenilghori4342@gmail.com>
2026-06-17 16:24:50 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,29 +1,32 @@
+// Bubble Sort:
+// Repeatedly compare adjacent elements and swap them
+// if they are in the wrong order until the array is sorted.
+
 public class BubbleSort {
 
     public static void main(String[] args) {
-        
-        int arr [] = {7,8,3,1,2};
 
-        for(int i=0; i < arr.length-1;i++){ // આ loop નક્કી કરે છે કે કેટલી વાર આખી array પર ફરીશું.
-            for(int j=0; j < arr.length-i-1; j++){ // આ loop બાજુબાજુના બે numbers ની સરખામણી કરે છે.
-                if(arr[j] > arr[j+1]){
-                    //Swap મોટા નંબરને એક પગલું પાછળ ધકેલે છે, અને ઘણા swaps પછી મોટો નંબર છેલ્લે પહોંચી જાય છે.
+        int arr[] = {7, 8, 3, 1, 2};
+
+        // Perform Bubble Sort
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            // Compare adjacent elements
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                // Swap if left element is greater
+                if (arr[j] > arr[j + 1]) {
 
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
 
-
-        // for print 
-
-        for(int i =0 ; i < arr.length;i++){
+        // Print sorted array
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
-
-
-    
 }
